03884d4443ec56a4902356b6d8a902c9d833fe08
DeepeshKalura <deepeshkalurs@gmail.com>
2024-10-11 07:13:39 +0530

    switch from writing myself decode encode to a go package
    but in this commit we will have error in the code

--- cmd/main.go
+++ cmd/main.go
@@ -1,142 +1,19 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
-	"sort"
-	"strconv"
 	"strings"
+
+	bencode "github.com/jackpal/bencode-go"
 )
 
 var ErrInvalidFormat = errors.New("invalid bencode format")
 
 // Function to decode bencoded data
-func decodeBencode(input string) (interface{}, string, error) {
-	if len(input) == 0 {
-		return nil, "", ErrInvalidFormat
-	}
-
-	switch input[0] {
-	case 'i':
-		return decodeInt(input)
-	case 'l':
-		return decodeList(input)
-	case 'd':
-		return decodeDict(input)
-	default:
-		if input[0] >= '0' && input[0] <= '9' {
-			return decodeString(input)
-		}
-	}
-
-	return nil, "", ErrInvalidFormat
-}
-
-func decodeInt(input string) (interface{}, string, error) {
-	if input[0] != 'i' {
-		return nil, "", ErrInvalidFormat
-	}
-
-	endIndex := strings.Index(input, "e")
-	if endIndex == -1 {
-		return nil, "", ErrInvalidFormat
-	}
-
-	numStr := input[1:endIndex]
-
-	// Check for leading zeros
-	if len(numStr) > 1 && (numStr[0] == '0' || (numStr[0] == '-' && numStr[1] == '0')) {
-		return nil, "", ErrInvalidFormat
-	}
-
-	num, err := strconv.Atoi(numStr)
-	if err != nil {
-		return nil, "", ErrInvalidFormat
-	}
-
-	return num, input[endIndex+1:], nil
-}
-
-func decodeString(input string) (interface{}, string, error) {
-	colonIndex := strings.Index(input, ":")
-	if colonIndex == -1 {
-		return nil, "", ErrInvalidFormat
-	}
-
-	lengthStr := input[:colonIndex]
-	length, err := strconv.Atoi(lengthStr)
-	if err != nil || length < 0 {
-		return nil, "", ErrInvalidFormat
-	}
-
-	if len(input) < colonIndex+1+length {
-		return nil, "", ErrInvalidFormat
-	}
-
-	strValue := input[colonIndex+1 : colonIndex+1+length]
-	return strValue, input[colonIndex+1+length:], nil
-}
-
-func decodeList(input string) (interface{}, string, error) {
-	if input[0] != 'l' {
-		return nil, "", ErrInvalidFormat
-	}
-
-	input = input[1:]
-	var list []interface{}
-
-	for input[0] != 'e' {
-		item, rest, err := decodeBencode(input)
-		if err != nil {
-			return nil, "", err
-		}
-		list = append(list, item)
-		input = rest
-	}
-
-	return list, input[1:], nil
-}
-
-func decodeDict(input string) (interface{}, string, error) {
-	if input[0] != 'd' {
-		return nil, "", ErrInvalidFormat
-	}
-
-	input = input[1:]
-	dict := make(map[string]interface{})
-	keys := []string{}
-
-	for input[0] != 'e' {
-		key, rest, err := decodeString(input)
-		if err != nil {
-			return nil, "", err
-		}
-
-		strKey, ok := key.(string)
-		if !ok {
-			return nil, "", ErrInvalidFormat
-		}
-
-		value, rest, err := decodeBencode(rest)
-		if err != nil {
-			return nil, "", err
-		}
-
-		dict[strKey] = value
-		keys = append(keys, strKey)
-		input = rest
-	}
-
-	sort.Strings(keys)
-	sortedDict := make(map[string]interface{})
-	for _, key := range keys {
-		sortedDict[key] = dict[key]
-	}
-
-	return sortedDict, input[1:], nil
-}
 
 func parseTorrentFile(filename string) (map[string]interface{}, error) {
 	result := make(map[string]interface{})
@@ -161,6 +38,10 @@ func parseTorrentFile(filename string) (map[string]interface{}, error) {
 			result["url"] = url
 		}
 
+		h := sha1.New()
+
+		h.Write(dict["info"].([]byte))
+		result["hash"] = h.Sum(nil)
 		if info, found := dict["info"]; found {
 			if infoDict, ok := info.(map[string]interface{}); ok {
 				result["size"] = infoDict["length"]
@@ -179,24 +60,19 @@ func main() {
 		fmt.Println("Usage: program <command> <argument>")
 		return
 	}
-
 	command := os.Args[1]
 
 	if command == "decode" {
 		bencodedValue := os.Args[2]
-		if bencodedValue == "le" {
-			fmt.Println("[]")
-			return
-		}
-		decode, _, err := decodeBencode(bencodedValue)
+		data, err := bencode.Decode(strings.NewReader(bencodedValue))
+
 		if err != nil {
-			fmt.Println(err)
-			return
+			panic(err)
 		}
-		jsonData, err := json.Marshal(decode)
+		jsonData, err := json.Marshal(data)
+
 		if err != nil {
-			fmt.Println("[]")
-			return
+			panic(err)
 		}
 		fmt.Println(string(jsonData))
 
@@ -207,6 +83,7 @@ func main() {
 		}
 		fmt.Printf("Tracker URL: %s\n", torrentDict["url"])
 		fmt.Printf("Length: %d\n", torrentDict["size"])
+		fmt.Printf("Info Hash: %x\n", torrentDict["hash"])
 
 	} else {
 		fmt.Printf("Unknown command: %s\n", command)
